d90991f2b7db89f480fcfa211b0504d6a55b3eb8
G3N1US9 <113584603+G3N1US9@users.noreply.github.com>
2023-12-09 11:48:31 +0300

    lr4

--- 1lab-1-3.php
+++ 1lab-1-3.php
@@ -1,18 +1,24 @@
 <?php
-// Создание константы NUM_E
-define("NUM_E", 2.71828);
-// Вывод значения константы
-echo "e EQUALS " . NUM_E . "<br>";
-// Присвоение переменной $num_e1 значения константы
+define('NUM_E', 2.71828);
+
+echo "Число e = " . NUM_E . " ";
+
+echo "\n";
 $num_e1 = NUM_E;
-// Вывод имени переменной, ее значения и типа
-echo "\$num_e1 = " . $num_e1 . " " . gettype($num_e1) . "<br>";
-// Изменение типа переменной $num_e1 на строковый и вывод значения и типа
-$num_e1 = (string)$num_e1;
-echo "\$num_e1 = " . $num_e1 . " " . gettype($num_e1) . "<br>";
-// Изменение типа переменной $num_e1 на целочисленный и вывод значения и типа
-$num_e1 = (int)$num_e1;
-echo "\$num_e1 = " . $num_e1 . " " . gettype($num_e1) . "<br>";
-// Изменение типа переменной $num_e1 на булевский и вывод значения и типа
-$num_e1 = (bool)$num_e1;
-echo "\$num_e1 = " . $num_e1 . " " . gettype($num_e1) . "<br>";
+
+echo "\n\$num_e1 = " . $num_e1 . " - ";
+var_dump($num_e1);
+
+$num_e1 = (string) $num_e1;
+echo "\$num_e1 = " . $num_e1 . " - ";
+var_dump($num_e1);
+
+$num_e1 = (int) $num_e1;
+echo "\$num_e1 = " . $num_e1 . " - ";
+var_dump($num_e1);
+
+$num_e1 = (bool) $num_e1;
+echo "\$num_e1 = " . $num_e1 . " - ";
+var_dump($num_e1);
+
+?>
